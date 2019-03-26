ac0fb172d75f4140c100df718a60289177b7cba2
designcandi <contact@mariecoyne.co.uk>
2019-03-26 10:01:49 +0000

    DataBase SQL Files
    Files to create the database used in the Add User website

---
+++ InsertUserForExistingEmailUnitTest.sql
@@ -0,0 +1,14 @@
+USE [TestAddUser]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([Email]
+           ,[Password]
+           ,[Salt])
+     VALUES
+           ('contact@mariecoyne.co.uk'
+           ,'koquAs3n88XP4Jkq8HvzCiO23LpyQxY1rXPr55i+fOs='
+           ,'t9jXtkN079BkFPtH')
+GO
+
+
